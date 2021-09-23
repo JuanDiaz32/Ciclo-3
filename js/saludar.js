d451a832ab748756203fc6f15b04b03ea45eca5f
Juan Pablo <juan.diaz3@o365.unab.edu.co>
2021-09-22 22:08:36 -0500

    Se agrega una página en js para registrar la compra de un producto

--- js/saludar.js
+++ js/saludar.js
@@ -1,5 +1,37 @@
-alert("Hola mundo segunda forma");
+//alert("Hola mundo segunda forma");
 
 console.log("Hola mundo desde consola");
 
 document.write("<h1>Hola mundo desde JavaScript</h1>");
+
+// Tipos de datos: String, Number, Boolean
+
+var nombre = `Juan`;
+let correo = 'juan@correo.com';
+const mayor = 18;
+let edad = 29;
+let altura = 1.81;
+let extranjero = false;
+
+let mensaje = "Hola,</br> soy "+nombre+" mi correo es "+correo+", tengo "+edad+" años";
+
+nombre = prompt("Escriba su nombre: ");
+correo = prompt("Escriba su correo: ");
+edad = parseInt(prompt("Escriba su edad: "));
+altura = parseFloat(prompt("Escriba su altura: "));
+
+let mensaje2 = `Hola, soy ${nombre} mi correo es ${correo}, tengo ${edad} años,
+ mido ${altura} metros`;
+
+if (edad>mayor) {
+    mensaje2+=`, soy mayor de edad`;
+} else if (edad<mayor) {
+    mensaje2+=`, soy menor de edad`;
+} else {
+    mensaje2+=`, tengo 18`;
+}
+
+// alert(variable);
+
+document.write("<h2>"+mensaje2+"</h2>");
+console.log("<h2>"+mensaje2+"</h2>");
